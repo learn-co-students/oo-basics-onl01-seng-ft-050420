f100b1bd12681596a3db052ba42322d10fa9ad3d
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-15 19:20:54 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book 
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+
+  def initialize(title)
+    @title = title
+  end  
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  
+  end
+  
+end  
 
 
+
+#title, author, page count etc 
+#keep track of all the genres of all the books we create
+#ability to turn their own pages
